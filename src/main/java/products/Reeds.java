1c054129b83b1b6f1af166a4561c9142bc4f09d6
Heather King <hkingdevelopment@gmail.com>
2022-01-14 14:55:44 +0000

    adds Reeds class and tests

--- src/main/java/products/Reeds.java
+++ src/main/java/products/Reeds.java
@@ -1,4 +1,31 @@
 package products;
 
-public class Reeds {
+import instruments.Instrument;
+
+public class Reeds extends Product {
+
+    private String strength;
+    private String brand;
+
+    public Reeds(String name, String description, Instrument instrument, double purchasePrice, double salePrice, String strength, String brand) {
+        super(name, description, instrument, purchasePrice, salePrice);
+        this.strength = strength;
+        this.brand = brand;
+    }
+
+    public String getStrength() {
+        return strength;
+    }
+
+    public void setStrength(String strength) {
+        this.strength = strength;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
--- src/test/java/ReedsTest.java
+++ src/test/java/ReedsTest.java
@@ -1,2 +1,104 @@
+import instruments.Clarinet;
+import instruments.ClassificationType;
+import instruments.Violin;
+import org.junit.Before;
+import org.junit.Test;
+import products.Reeds;
+
+import static org.junit.Assert.assertEquals;
+
 public class ReedsTest {
+
+    Reeds reeds;
+    Clarinet clarinet;
+    Violin violin;
+
+    @Before
+    public void before() {
+        clarinet = new Clarinet("Selmer", "CL211", "Intermediate", ClassificationType.WOODWIND, "BFlat", "Wood", "Felt");
+        violin = new Violin("Stentor", "1018 Student", "Standard", ClassificationType.STRING, "4/4", "Helicore");
+        reeds = new Reeds("Rico Royal", "box of 10", clarinet, 15.00, 30.00, "3b", "Rico");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Rico Royal", reeds.getName());
+    }
+
+    @Test
+    public void canSetName() {
+        reeds.setName("Vandoren");
+        assertEquals("Vandoren", reeds.getName());
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("box of 10", reeds.getDescription());
+    }
+
+    @Test
+    public void canSetDescription() {
+        reeds.setDescription("box of 5");
+        assertEquals("box of 5", reeds.getDescription());
+    }
+
+    @Test
+    public void hasInstrument() {
+        assertEquals(clarinet, reeds.getInstrument());
+    }
+
+    @Test
+    public void canSetInstrument() {
+        reeds.setInstrument(violin);
+        assertEquals(violin, reeds.getInstrument());
+    }
+
+    @Test
+    public void hasPurchasePrice() {
+        assertEquals(15.00, reeds.getPurchasePrice(), 0.0);
+    }
+
+    @Test
+    public void canSetPurchasePrice() {
+        reeds.setPurchasePrice(10.00);
+        assertEquals(10.00, reeds.getPurchasePrice(), 0.0);
+    }
+
+    @Test
+    public void hasSalePrice() {
+        assertEquals(30.00, reeds.getSalePrice(), 0.0);
+    }
+
+    @Test
+    public void canSetSalePrice() {
+        reeds.setSalePrice(20.00);
+        assertEquals(20.00, reeds.getSalePrice(), 0.0);
+    }
+
+    @Test
+    public void hasStrength() {
+        assertEquals("3b", reeds.getStrength());
+    }
+
+    @Test
+    public void canSetStrength() {
+        reeds.setStrength("2a");
+        assertEquals("2a", reeds.getStrength());
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Rico", reeds.getBrand());
+    }
+
+    @Test
+    public void canSetBrand() {
+        reeds.setBrand("Vandoren");
+        assertEquals("Vandoren", reeds.getBrand());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(15.00, reeds.calculateMarkup(), 0.0);
+    }
 }
