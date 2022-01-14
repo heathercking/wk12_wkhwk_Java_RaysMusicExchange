2466149d19c219cde566e358af22fcbf6a0d84e5
Heather King <hkingdevelopment@gmail.com>
2022-01-14 13:05:13 +0000

    adds Timpani tests and class

--- src/main/java/instruments/Timpani.java
+++ src/main/java/instruments/Timpani.java
@@ -1,4 +1,23 @@
 package instruments;
 
-public class Timpani {
+public class Timpani extends Instrument {
+
+    private int size;
+
+    public Timpani(String brand, String model, String range, ClassificationType classification, int size) {
+        super(brand, model, range, classification);
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String play() {
+        return "bang bang!";
+    }
 }
--- src/test/java/TimpaniTest.java
+++ src/test/java/TimpaniTest.java
@@ -1,2 +1,76 @@
+import instruments.ClassificationType;
+import instruments.Timpani;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TimpaniTest {
+
+    Timpani timpani;
+
+    @Before
+    public void before() {
+        timpani = new Timpani("Adams", "TP123", "Beginner", ClassificationType.PERCUSSION, 32);
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Adams", timpani.getBrand());
+    }
+
+    @Test
+    public void canSetBrand() {
+        timpani.setBrand("Yamaha");
+        assertEquals("Yamaha", timpani.getBrand());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("TP123", timpani.getModel());
+    }
+
+    @Test
+    public void canSetModel() {
+        timpani.setModel("CL111");
+        assertEquals("CL111", timpani.getModel());
+    }
+
+    @Test
+    public void hasRange() {
+        assertEquals("Beginner", timpani.getRange());
+    }
+
+    @Test
+    public void canSetRange() {
+        timpani.setRange("Intermediate");
+        assertEquals("Intermediate", timpani.getRange());
+    }
+
+    @Test
+    public void hasClassification() {
+        assertEquals(ClassificationType.PERCUSSION, timpani.getClassification());
+    }
+
+    @Test
+    public void canSetClassification() {
+        timpani.setClassification(ClassificationType.BRASS);
+        assertEquals(ClassificationType.BRASS, timpani.getClassification());
+    }
+
+    @Test
+    public void hasSize() {
+        assertEquals(32, timpani.getSize());
+    }
+
+    @Test
+    public void canSetSize() {
+        timpani.setSize(29);
+        assertEquals(29, timpani.getSize());
+    }
+    
+    @Test
+    public void canPlay() {
+        assertEquals("bang bang!", timpani.play());
+    }
 }
