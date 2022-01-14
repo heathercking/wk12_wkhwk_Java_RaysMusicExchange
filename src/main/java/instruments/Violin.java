e566956ea3f35e2c43585838a74a428a2043bc30
Heather King <hkingdevelopment@gmail.com>
2022-01-14 12:58:31 +0000

    adds Violin tests and class

--- src/main/java/instruments/Violin.java
+++ src/main/java/instruments/Violin.java
@@ -1,4 +1,33 @@
 package instruments;
 
-public class Violin {
+public class Violin extends Instrument {
+
+    private String size;
+    private String stringMake;
+
+    public Violin(String brand, String model, String range, ClassificationType classification, String size, String stringMake) {
+        super(brand, model, range, classification);
+        this.size = size;
+        this.stringMake = stringMake;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getStringMake() {
+        return stringMake;
+    }
+
+    public void setStringMake(String stringMake) {
+        this.stringMake = stringMake;
+    }
+
+    public String play() {
+        return "squeek squeek!";
+    }
 }
--- src/test/java/ViolinTest.java
+++ src/test/java/ViolinTest.java
@@ -1,2 +1,88 @@
+import instruments.ClassificationType;
+import instruments.Violin;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViolinTest {
+
+    Violin violin;
+
+    @Before
+    public void before() {
+        violin = new Violin("Stentor", "1018 Student", "Standard", ClassificationType.STRING, "4/4", "Helicore");
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Stentor", violin.getBrand());
+    }
+
+    @Test
+    public void canSetBrand() {
+        violin.setBrand("Yamaha");
+        assertEquals("Yamaha", violin.getBrand());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("1018 Student", violin.getModel());
+    }
+
+    @Test
+    public void canSetModel() {
+        violin.setModel("CL111");
+        assertEquals("CL111", violin.getModel());
+    }
+
+    @Test
+    public void hasRange() {
+        assertEquals("Standard", violin.getRange());
+    }
+
+    @Test
+    public void canSetRange() {
+        violin.setRange("Beginner");
+        assertEquals("Beginner", violin.getRange());
+    }
+
+    @Test
+    public void hasClassification() {
+        assertEquals(ClassificationType.STRING, violin.getClassification());
+    }
+
+    @Test
+    public void canSetClassification() {
+        violin.setClassification(ClassificationType.BRASS);
+        assertEquals(ClassificationType.BRASS, violin.getClassification());
+    }
+
+    @Test
+    public void hasSize() {
+        assertEquals("4/4", violin.getSize());
+    }
+
+    @Test
+    public void canSetSize() {
+        violin.setSize("1/4");
+        assertEquals("1/4", violin.getSize());
+    }
+
+    @Test
+    public void hasStringMake() {
+        assertEquals("Helicore", violin.getStringMake());
+    }
+
+    @Test
+    public void canSetStringMake() {
+        violin.setStringMake("Pirastro");
+        assertEquals("Pirastro", violin.getStringMake());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("squeek squeek!", violin.play());
+    }
+
 }
